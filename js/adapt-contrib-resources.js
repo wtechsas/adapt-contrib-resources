a14f929d4c6927eb7660decadf8a106e28127f18
Mario Carreño <macb0797@gmail.com>
2022-07-16 10:13:06 -0500

    Plugin traducido

---
+++ js/adapt-contrib-resources.js
@@ -0,0 +1,43 @@
+import Adapt from 'core/js/adapt';
+import drawer from 'core/js/drawer';
+import ResourcesView from './ResourcesView';
+import './ResourcesHelpers';
+
+class Resources extends Backbone.Controller {
+
+  initialize() {
+    this.listenTo(Adapt, 'adapt:start', this.initResources);
+  }
+
+  initResources() {
+    const courseResources = Adapt.course.get('_resources');
+
+    // do not proceed until resource set on course.json
+    if (!courseResources || courseResources._isEnabled === false) return;
+
+    const drawerObject = {
+      title: courseResources.title,
+      description: courseResources.description,
+      className: 'is-resources',
+      drawerOrder: courseResources._drawerOrder || 0
+    };
+
+    drawer.addItem(drawerObject, 'resources:showResources');
+
+    this.setupResources(courseResources);
+  }
+
+  setupResources(resourcesData) {
+    const model = new Backbone.Model(resourcesData);
+    const collection = new Backbone.Collection(model.get('_resourcesItems'));
+
+    this.listenTo(Adapt, 'resources:showResources', () => {
+      drawer.triggerCustomView(new ResourcesView({
+        model,
+        collection
+      }).$el);
+    });
+  }
+}
+
+export default new Resources();
